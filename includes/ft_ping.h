0861e214b5b1df5d82c16f0ca1030da5353ad7cb
Joffrey Janiec <jjaniec@student.42.fr>
2019-10-27 14:13:42 +0100

    icmp echo

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -19,10 +19,12 @@
 #ifndef FT_PING_H
 # define FT_PING_H
 
-# define GETADDRINFO_RESOLVE_PROTO	"http"
-# define SOCK_TYPE					SOCK_RAW
-# define SOCK_PROTO					IPPROTO_ICMP
-# define ICMP_ECHO_SEQ_COUNT		5
+# define FT_PING_GETADDRINFO_RESOLVE_PROTO	"http"
+# define FT_PING_SOCK_TYPE					SOCK_RAW
+# define FT_PING_SOCK_PROTO					IPPROTO_ICMP
+# define FT_PING_ICMP_ECHO_SEQ_COUNT		5
+
+# define FT_PING_BUFF_LEN           2000
 
 unsigned short		calc_checksum(void *b, int len);
 
--- srcs/main.c
+++ srcs/main.c
@@ -39,7 +39,6 @@ static int			resolve_hostname(char *hostname, struct in_addr *ip)
 {
 	struct addrinfo		hints;
 	struct addrinfo		*res;
-	struct addrinfo		*res_ptr;
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
@@ -49,10 +48,10 @@ static int			resolve_hostname(char *hostname, struct in_addr *ip)
 		printf("getaddrinfo() failed!\n");
 		return (1);
 	}
-	res_ptr = res;
+	// res_ptr = res;
 	// while (res_ptr)
 	// {
-	// 	printf("Resolved to: %s\n", \
+	// 	printf("Resolved to: %s\n", 
 	// 		inet_ntoa((((struct sockaddr_in *)res_ptr->ai_addr)->sin_addr)));
 	// 	res_ptr = res_ptr->ai_next;
 	// }
@@ -61,18 +60,86 @@ static int			resolve_hostname(char *hostname, struct in_addr *ip)
 }
 
 /*
+** Send icmp echo to ip address
+**
+** https://www.tenouk.com/Module43.html
+** http://download.opendds.org/doxygen/ace_tao/html/libace-doc/a00839.html
+*/
+
+
+static ssize_t		send_icmp_echo(int s, struct sockaddr_in *addr)
+{
+	char            buff[FT_PING_BUFF_LEN];
+	struct icmp     *icmp_out;
+
+	icmp_out = (struct icmp *)buff;
+	icmp_out->icmp_type = ICMP_ECHO;
+	icmp_out->icmp_code = 0;
+	icmp_out->icmp_id = getpid();
+	icmp_out->icmp_seq = htons(1);
+	icmp_out->icmp_cksum = 0;
+	ft_memcpy(buff, icmp_out, sizeof(icmp_out));
+	icmp_out->icmp_cksum = calc_checksum(buff, sizeof(icmp_out) + 1);
+	ft_memcpy(buff, icmp_out, sizeof(icmp_out));
+	return (
+		sendto(s, buff, sizeof(icmp_out) + 1, 0, (struct sockaddr *)addr, sizeof(*addr))
+	);
+}
+
+/*
+** Send ICMP echo requests & wait for icmp echo replies
+*/
+
+static int          ft_ping(int s, struct sockaddr_in *addr)
+{
+	ssize_t		bytes_sent;
+
+	if ((bytes_sent = send_icmp_echo(s, addr)) <= 0)
+	{
+		dprintf(2, "sendto() failed\n");
+	}
+	else
+	{
+		printf("Sent %zu bytes", bytes_sent);
+	}
+	return (0);
+}
+
+/*
+** Init socket & set socket options
+*/
+
+static int          init_socket(int *s)
+{
+	int         r;
+	int   		on;
+
+	r = 0;
+	on = 255;
+	if ((*s = socket(PF_INET, FT_PING_SOCK_TYPE, FT_PING_SOCK_PROTO)) < 0)
+	{
+		dprintf(2, "socket() error, are you root ?\n");
+		r = 1;
+	}
+	else if (setsockopt(*s, IPPROTO_IP, IP_TTL, &on, sizeof(on)) != 0)
+	{
+		dprintf(2, "Failed to set TTL option on socket\n");
+		r = 1;
+	}
+	return (r);
+}
+
+/*
 ** in_addr: https://apr.apache.org/docs/apr/trunk/structin__addr.html
 ** sockaddr_in: https://docs.huihoo.com/doxygen/linux/kernel/3.7/structsockaddr__in.html
-** 
 */
 
 int		main(int ac, char **av)
 {
 	struct in_addr			ip;
 	struct sockaddr_in		addr;
-	// struct in_addr			dst;
+	int                     s;
 
-	(void)ac;
 	if (ac != 2)
 	{
 		printf("Usage: %s destination_ip\n", av[0]);
@@ -80,11 +147,11 @@ int		main(int ac, char **av)
 	}
 	else if (resolve_hostname(av[1], &ip))
 	{
-		printf("%s: cannot resolve %s: Unknown host\n", av[0], av[1]);
+		dprintf(2, "%s: cannot resolve %s: Unknown host\n", av[0], av[1]);
 		return (1);
 	}
 	addr.sin_family = AF_INET;
 	addr.sin_addr = ip;
 	printf("PING %s (%s)\n", av[1], inet_ntoa(ip));
-	return (0);
+	return (init_socket(&s) || ft_ping(s, &addr));
 }
